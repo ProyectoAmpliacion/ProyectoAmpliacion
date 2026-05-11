b3ad62dd9c32589317a4f4b6c40eef48ac5be8b8
ProyectoAmpliacion <giulia.gherghin@educa.madrid.org>
2026-05-11 22:10:53 +0200

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h1>¡Hola WebFusion!</h1>";
-echo "<h3>Integrantes del proyecto:</h3>";
+echo "<h3>Integrantes del proyecto: </h3>";
 echo "<ul>";
 echo "<li>Giulia Alisia Gherghin</li>";
 echo "<li>Marta Pérez García</li>";
