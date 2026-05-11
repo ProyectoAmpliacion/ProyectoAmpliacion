20402ab526cd01c11cb6034c18c39c205003902f
ProyectoAmpliacion <giulia.gherghin@educa.madrid.org>
2026-05-11 21:53:34 +0200

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 echo "<h1>¡Hola WebFusion!</h1>";
+echo "<h3>Integrantes del proyecto:</h3>";
+echo "<ul>";
+echo "<li>Giulia Alisia Gherghin</li>";
+echo "<li>Marta Pérez García</li>";
+echo "<li>Laura Gema Moreno</li>";
+echo "</ul>";
 echo "<p>Entorno WordPress desplegado con éxito mediante Vagrant, Docker y Git.</p>";
-phpinfo();
 ?>
